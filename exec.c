451067b0ed1021668ec9906b2197030ee2b1a407
tjennat <theo.jennat98@gmail.com>
2023-12-15 01:35:48 -0800

    exec function call by shell.c

---
+++ exec.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+extern char **environ;
+
+void exec(char **args)
+{
+	int status;
+	pid_t childPid;
+
+	childPid = fork();
+	if (childPid == -1)
+		perror("fork");
+
+	else if (childPid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(&status);
+}
